2a330bbf8ca77c2cf0716f90761805782139fb38
meetpatel2531 <meet25patel31@gmail.com>
2020-12-03 12:58:56 +0530

    ratu live app backed

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', msg => {
+        console.log(msg);
+        io.emit("chat message", msg);
+    })
+  });
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
